a2c47641025c5455fe03e2d08ba444600a2784f7
Anibal <anibalmreinoso@gmail.com>
2023-04-29 13:09:55 -0300

    comienzo de proyecto

---
+++ index.js
@@ -0,0 +1,51 @@
+var años = 23;
+let edad = "23";
+const tamaño = '160cm';
+let llovera = false;
+
+let array = [2,4,6,7,0];
+
+let objeto = {
+    "nombre": "Eduardo",
+    "edad": 34,
+    "hobbies": ["futbol", "jugar a la pc", "cocinar"],
+    "hijos": [{
+        "nombre": "Dario",
+        "edad": 11
+    },
+    {
+        "nombre": "Julieta",
+        "edad": 15
+    }]
+};
+
+// console.log(objeto);
+
+// if (condicion){
+//     se cumple una condicion
+// }
+// else if(condicion2) {
+//     se cumple la segunda condicion
+// }
+// else{
+//     condicion 3
+// }
+
+// console.log(años != edad || años === edad);
+
+// let saludo = "Hola,";
+// let saludo2 = " mundo!";
+
+// let frase = saludo += saludo2;
+
+// console.log(saludo)
+
+// let nombres = ["Andrea", "Juan", "Camila", "Antonio"]
+
+
+// let nombre = alert("Ingrese un nombre: ");
+// nombres.push(nombre);
+
+
+// console.log(nombres);
+
